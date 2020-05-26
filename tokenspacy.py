551972a9178a8196dc1cb5d1d57d6aa70649ecf0
SOMIL JAIN <42209500+somiljain7@users.noreply.github.com>
2020-05-27 01:22:17 +0530

    #3

--- tokenspacy.py
+++ tokenspacy.py
@@ -3,3 +3,7 @@ nlp = spacy.load('en')
 doc = nlp("Tea is healthy and calming, don't you think?")
 for token in doc:
     print(token)
+print(f"Token \t\tLemma \t\tStopword".format('Token', 'Lemma', 'Stopword'))
+print("-"*40)
+for token in doc:
+    print(f"{str(token)}\t\t{token.lemma_}\t\t{token.is_stop}")
